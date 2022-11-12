9080ed582f5928ba1c683018a2da7a80799a7c94
anunadh.rajesh <anunadhrajesh777@gmail.com>
2022-11-12 11:46:50 +0530

    Add project files.

---
+++ TechnicalLab12-11-22/Employee.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicalLab12_11_22
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public string Department { get; set; }
+
+        private string _designation { get; set; }
+
+        public string Designation
+        {
+            get
+            {
+                return _designation;
+            }
+            set
+            {
+                if (value == "Trainee")
+                {
+                    salary = 5000;
+                    _designation = value;
+
+                }
+                else if (value == "Manager")
+                {
+                    salary = 200000;
+                    _designation = value;
+
+                }
+                else if (value == "Senior Developer")
+                {
+                    salary = 100000;
+                    _designation = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+
+            }
+        }
+        private int salary { get; set; }
+
+
+        public int Salary { get { return salary; } set { salary = value; } }
+
+        public void Getdata()
+        {
+            Console.WriteLine("Enter the Name");
+            Name = Console.ReadLine();
+            Console.WriteLine("Enter the Department");
+            Department = Console.ReadLine();
+            Console.WriteLine("Enter the Designation");
+            Designation = Console.ReadLine();
+        }
+    }
+}
+
---
+++ TechnicalLab12-11-22/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TechnicalLab12_11_22;
+
+var Manager = new Employee();
+Manager.Getdata();
+Console.WriteLine("Salary " + Manager.Salary);
